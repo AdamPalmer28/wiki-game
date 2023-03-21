457e720a7ed27088360a513427a8b347e1753841
AdamPalmer28 <93833080+AdamPalmer28@users.noreply.github.com>
2023-03-21 23:22:00 +0000

    skeleton structure added

---
+++ src/wiki_network/wiki_page.py
@@ -0,0 +1,44 @@
+"""
+This module contains the class for a wiki page.
+
+This class is used to extract the content from a wiki page.
+"""
+
+
+
+class page_extract:
+    """
+    Extract the content from a wiki page
+    """
+    
+    def __init__(self, content, wiki_url: str):
+
+        self.get_artical(content) # filter page content
+
+        self.wiki_url = wiki_url
+
+    def get_links(self):
+        """
+        Get all links from page content
+        """
+        pass
+
+    def get_headers(self):
+        """
+        Get all headers from the wiki artical
+        With the associated links of each section/header
+        """
+        pass
+
+    def summarise(self):
+        """
+        Summarise the page content into key words for 
+        """
+        pass
+
+    def get_artical(self, content):
+        """
+        Get the artical from wiki page, removing all irrelevant html
+        elements which are not going to yeild any useful information
+        """
+        pass
